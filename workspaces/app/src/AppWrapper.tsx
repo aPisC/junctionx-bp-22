c7cb0bfaa9335334edb21db4efd7d1e673d2fc92
makaias <sandorattilamakai@gmail.com>
2022-10-21 20:29:42 +0100

    added basic frontend dependencies, set up tailwindcss created wrapper to imitate mobile view on desktop

---
+++ workspaces/app/src/AppWrapper.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, ReactNode } from 'react'
+
+interface WindowSizeProps {
+  width: number
+  height: number
+}
+
+interface AppWrapperProps {
+  children: ReactNode
+}
+
+function AppWrapper({ children }: AppWrapperProps) {
+  const [windowSize, setWindowSize] = useState<WindowSizeProps>({ height: 0, width: 0 })
+  useEffect(() => {
+    const handleResize = () => {
+      let newHeight = window.innerHeight * 0.8
+      let newWidth = newHeight * 0.45
+      if (newWidth > window.innerWidth) {
+        const scale = window.innerWidth / newWidth
+        newHeight = newHeight * scale
+        newWidth = newWidth * scale
+      }
+      setWindowSize({ width: newWidth, height: newHeight })
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  }, [window.innerHeight, window.innerWidth])
+  return (
+    <div
+      style={{
+        width: windowSize.width,
+        height: windowSize.height,
+      }}
+      className="p-4"
+    >
+      <div
+        className={`w-full h-full bg-pink-500 relative rounded-lg border-x-4 overflow-hidden border-y-8 border-black`}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default AppWrapper
--- workspaces/app/src/main.tsx
+++ workspaces/app/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { isMobile } from 'react-device-detect'
+import AppWrapper from './AppWrapper'
+
+export interface MobileWrapperProps {
+  children: ReactNode
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {isMobile ? (
+      <App />
+    ) : (
+      <AppWrapper>
+        <App />
+      </AppWrapper>
+    )}
   </React.StrictMode>
 )
