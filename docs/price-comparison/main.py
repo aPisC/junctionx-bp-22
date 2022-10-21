e73ddb4863132826cb257a5448e70064f79e3444
Marcell O <omarcell@yahoo.com>
2022-10-21 21:03:44 +0200

    Started writing calc function

--- docs/price-comparison/main.py
+++ docs/price-comparison/main.py
@@ -1,1 +1,15 @@
 print("Hello world")
+
+def calc():
+    """
+    Function to calculate relative price indices between
+    two countries
+
+    Inputs:
+
+    Returns:
+    
+    """
+
+if __name__ == "__main__":
+    calc()
