f26c9d79f5c9b0c3c7c03e4af0e33c6cb2d59d53
Bendeguz Cseresnyes <cser.bende@gmail.com>
2022-10-22 17:54:54 +0000

    Implement target selector screen

--- workspaces/app/src/pages/comparisonSelector/index.tsx
+++ workspaces/app/src/pages/comparisonSelector/index.tsx
@@ -1,17 +1,44 @@
+import axios from 'axios'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { flags } from '../../config/flags'
+import { BACKEND_URL } from '../../config/backendUrl'
 import Button from '../../modules/button'
 import { FlagSelector } from '../../modules/flagSelector'
 import { H1 } from '../../modules/h1'
 import Navigation from '../../modules/navigation'
+import { useSpinnerOverlay } from '../../utils/SipnnerOverlay/useSpinnerOverlay'
+import { useSpinneredRequest } from '../../utils/useSpinneredRequest'
+import { useTriggeredRequest } from '../../utils/useTriggeredRequest'
 import BasePage from '../base'
 
 export interface ComparisonSelectorPageProps {}
 
 export default function ComparisonSelectorPage({}: ComparisonSelectorPageProps) {
-  const [flag, setFlag] = useState<typeof flags[0] | null>(null)
   const navigate = useNavigate()
+  const [flag, setFlag] = useState<string>('de')
+
+  const userRequest = useSpinneredRequest(
+    () =>
+      axios(`${BACKEND_URL}/api/user/${localStorage.getItem('token')}`)
+        .then((d) => {
+          if (d.data.targetCountry) navigate('/comparison-dashboard')
+          return d.data
+        })
+        .catch((e) => {
+          localStorage.removeItem('token')
+          navigate('/')
+        }),
+    []
+  )
+
+  const setTargetCountryRequest = useTriggeredRequest(async () => {
+    axios.post(`${BACKEND_URL}/api/user/setTarget/${flag}/${userRequest.data.id}`)
+    navigate('/comparison-dashboard')
+  })
+  useSpinnerOverlay(setTargetCountryRequest.isRunning)
+
+  if (userRequest.isRunning) return null
+
   return (
     <BasePage>
       <div className="h-full flex flex-col">
@@ -19,19 +46,14 @@ export default function ComparisonSelectorPage({}: ComparisonSelectorPageProps) 
         <div className="flex flex-grow overflow-hidden">
           <div className="p-4 w-full h-full flex flex-col justify-evenly items-center">
             <H1>Home Country</H1>
-            <FlagSelector
-              disabled
-              flag={flags.find((flag) => localStorage.getItem('flag') === flag.country) as any}
-              setFlagCallback={setFlag}
-            />
+            <FlagSelector disabled flag={userRequest.data?.sourceCountry} setFlagCallback={setFlag} />
             <H1 className="text-center">Destination Country</H1>
-            <FlagSelector flag={flag as any} setFlagCallback={setFlag} />
+            <FlagSelector flag={flag} setFlagCallback={setFlag} />
             <Button
               disabled={flag == null}
               rounded
               onClick={() => {
-                localStorage.setItem('destination', flag?.country ?? '')
-                navigate('/comparison-dashboard')
+                setTargetCountryRequest.run()
               }}
             >
               Continue
