33d5fd74c7a1f838098592aeb39dd92b6e0e35c9
Marcell O <omarcell@yahoo.com>
2022-10-22 16:25:46 +0200

    Added file changes

--- docs/price-comparison/calc.py
+++ docs/price-comparison/calc.py
@@ -1,6 +1,54 @@
 import numpy as np
 import pandas as pd
 
+def master_calculator(source_id: str,
+                      target_id: str,
+                      category: str,
+                      currencies_by_country: dict,
+                      exchange_rate_info: list,
+                      country_category_ppp_data) -> float:
+    """
+    Function to calculate living costs in different countries
+    
+    Inputs:
+    source_id: Source country identifier e.g.: 'hu'
+    
+    target_id: Target country identifier e.g.: 'at'
+    
+    category: Living expense category to be compared
+    
+    currencies_by_country: Dictionary listing currency
+    unit in each country. Keys are country IDs
+    
+    exchange_rate_info: Wise API response, listing
+    exchange rates in different currencies/countries
+    
+    country_category_ppp_data: Dataframe containing
+    the PPP (compared to EU27 avg) in each country
+    
+    Returns:
+    price_ratio: (float) multiplier for living expenses
+    price_ratio > 1: home country more expensive
+    price_ratio < 1: home country cheaper
+    """
+
+    # Retrieve PPP values
+    home_ppp = country_category_ppp_data[
+        country_category_ppp_data["country_id"] == source_id
+        & country_category_ppp_data["category"] == category]["value"].iloc[0]
+
+    foreign_ppp = country_category_ppp_data[
+        country_category_ppp_data["country_id"] == target_id
+        & country_category_ppp_data["category"] == category]["value"].iloc[0]
+
+    # Retrieve currency conversion info
+    exchange_ratio = exchange_rate_finder(exchange_rate_info,
+        currencies_by_country[target_id],
+        currencies_by_country[source_id])
+
+    # Do calculation
+    price_ratio = home_ppp / foreign_ppp * exchange_ratio
+    return price_ratio
 
 def calc_category_index(values, weights=None) -> float:
     """
