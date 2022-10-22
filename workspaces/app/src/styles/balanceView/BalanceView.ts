326692e6f287f46c7bc81f03949f10b427da9bd9
makaias <sandorattilamakai@gmail.com>
2022-10-22 01:13:51 +0100

    implemented balance view for home page

---
+++ workspaces/app/src/styles/balanceView/BalanceView.ts
@@ -0,0 +1,22 @@
+export const BalanceViewStyle = {
+  container: {
+    flex: 'flex',
+    flexDirection: 'flex-col',
+  },
+  item: {
+    flex: 'flex',
+    w: 'w-full',
+    xPosition: 'justify-center',
+  },
+  variants: {
+    base: {
+      default: {
+        color: 'text-white',
+        textSize: 'text-xl',
+      },
+      value: {
+        color: 'text-white',
+      },
+    },
+  },
+}
