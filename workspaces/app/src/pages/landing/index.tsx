835501ab365384ae4c54511c4cde9de8ff6dd3e3
makaias <sandorattilamakai@gmail.com>
2022-10-22 03:52:23 +0100

    resolved merge conflict

--- workspaces/app/src/pages/landing/index.tsx
+++ workspaces/app/src/pages/landing/index.tsx
@@ -8,18 +8,11 @@ export interface LandingPageProps {}
 export default function LandingPage({}: LandingPageProps) {
   return (
     <div className="p-4 w-full h-full flex flex-col justify-evenly items-center">
-      <Modal blur rounded>
-        <H1>Home Country</H1>
-        <ModalHandler>
-          <div className="w-full flex items-center justify-center">
-            <FlagButton className="w-[50%]" flag="asdf">
-              <img src="" alt="" />
-            </FlagButton>
-          </div>
-        </ModalHandler>
-        <ModalBody>asdf</ModalBody>
-      </Modal>
-      <Button rounded>Continue</Button>
+      <H1>Home Country</H1>
+      <FlagSelector />
+      <a href="/home">
+        <Button rounded>Continue</Button>
+      </a>
     </div>
   )
 }
