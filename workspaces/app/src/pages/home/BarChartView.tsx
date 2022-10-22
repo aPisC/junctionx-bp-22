36ce6d2505249c56ae0395cf9120f117f2993b90
makaias <sandorattilamakai@gmail.com>
2022-10-22 01:18:13 +0100

    introduced react charts, created barchart view for base page categories

---
+++ workspaces/app/src/pages/home/BarChartView.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js'
+import { Bar } from 'react-chartjs-2'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    y: {
+      display: false,
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+}
+
+export interface BarChartViewProps {
+  labels: string[]
+  datasets: any
+}
+
+export const BarChartView = ({ labels, datasets }: BarChartViewProps) => {
+  const data = {
+    labels,
+    datasets,
+  }
+  return (
+    <div className="w-1/3">
+      <Bar options={options} data={data} />
+    </div>
+  )
+}
