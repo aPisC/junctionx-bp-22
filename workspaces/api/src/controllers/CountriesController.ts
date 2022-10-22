7357bb3ad40be3cdb3997ac01d7e3732cb1448e5
Bendeguz Cseresnyes <cser.bende@gmail.com>
2022-10-22 12:25:27 +0000

    update countries query

--- workspaces/api/src/controllers/CountriesController.ts
+++ workspaces/api/src/controllers/CountriesController.ts
@@ -6,20 +6,23 @@ import { injectable } from 'tsyringe'
 export default class CountriesController {
   @Route.Get('/')
   getCountries() {
-    return Object.keys(countries)
+    return Object.values(countries)
   }
 }
 
 export const countries: { [key: string]: CountryDescription } = {
-  es: { currency: 'EUR' },
-  at: { currency: 'EUR' },
-  fr: { currency: 'EUR' },
-  dk: { currency: 'DKK' },
-  se: { currency: 'SEK' },
-  de: { currency: 'EUR' },
-  hu: { currency: 'HUF' },
+  es: { id: 'es', currency: 'EUR', image: '/file/es.svg', country: 'Spain' },
+  at: { id: 'at', currency: 'EUR', image: '/file/at.svg', country: 'Austria' },
+  fr: { id: 'fr', currency: 'EUR', image: '/file/fr.svg', country: 'France' },
+  dk: { id: 'dk', currency: 'DKK', image: '/file/dk.svg', country: 'Denmark' },
+  se: { id: 'se', currency: 'SEK', image: '/file/se.svg', country: 'Sweden' },
+  de: { id: 'de', currency: 'EUR', image: '/file/de.svg', country: 'Germany' },
+  hu: { id: 'hu', currency: 'HUF', image: '/file/hu.svg', country: 'Hungary' },
 }
 
 interface CountryDescription {
   currency: string
+  country: string
+  id: string
+  image: string
 }
