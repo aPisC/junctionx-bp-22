e5c6a6b457e2ea2b956ee08327b4eb089db44b04
makaias <sandorattilamakai@gmail.com>
2022-10-22 04:06:27 +0100

    added routing to exsisting pages, fixed flex grow problem

--- workspaces/app/src/pages/base/index.tsx
+++ workspaces/app/src/pages/base/index.tsx
@@ -1,18 +1,9 @@
 import { ReactNode } from 'react'
-import Navigation from '../../modules/navigation'
-import { Scrollbars } from 'react-custom-scrollbars-2'
 
 export interface BasePageProps {
   children: ReactNode
 }
 
 export default function BasePage({ children }: BasePageProps) {
-  return (
-    <div className="h-full overflow-hidden">
-      <Scrollbars>
-        <Navigation />
-        {children}
-      </Scrollbars>
-    </div>
-  )
+  return <div className="h-full overflow-hidden">{children}</div>
 }
--- workspaces/app/src/pages/hub/index.tsx
+++ workspaces/app/src/pages/hub/index.tsx
@@ -1,5 +1,6 @@
 import Button from '../../modules/button'
 import { H1 } from '../../modules/h1'
+import Navigation from '../../modules/navigation'
 import BasePage from '../base'
 
 export interface HubPageProps {}
@@ -7,16 +8,21 @@ export interface HubPageProps {}
 export default function HubPage({}: HubPageProps) {
   return (
     <BasePage>
-      <div className="flex flex-col gap-2 p-2 w-full">
-        <Button variant="hub">
-          <H1>Compare</H1>
-        </Button>
-        <Button variant="hub">
-          <H1>Plan</H1>
-        </Button>
-        <Button variant="hub">
-          <H1>Move</H1>
-        </Button>
+      <div className="h-full flex flex-col">
+        <Navigation />
+        <div className="flex-grow overflow-hidden">
+          <div className="flex flex-col gap-2 p-2">
+            <Button variant="hub">
+              <H1>Compare</H1>
+            </Button>
+            <Button variant="hub">
+              <H1>Plan</H1>
+            </Button>
+            <Button variant="hub">
+              <H1>Move</H1>
+            </Button>
+          </div>
+        </div>
       </div>
     </BasePage>
   )
