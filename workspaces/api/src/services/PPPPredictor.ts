1fbab021e40b0e54dd7fd152ad31e9484d1f5e15
Bendeguz Cseresnyes <cser.bende@gmail.com>
2022-10-22 19:02:09 +0000

    add PPP csv

--- workspaces/api/src/services/PPPPredictor.ts
+++ workspaces/api/src/services/PPPPredictor.ts
@@ -1,8 +1,37 @@
+import fs from 'fs'
+import path from 'path'
+
 export default class PPPPredictor {
-  constructor(private sourceCountry: string, private targetCountry: string) {}
+  private lookup: any
+  constructor(private sourceCountry: string, private targetCountry: string) {
+    this.lookup = data[sourceCountry]?.[targetCountry]
+    if (!this.lookup) throw new Error('unable to initialize pppPredictor')
+  }
 
   getPrice(category: string, amount: number) {
-    if (this.sourceCountry == 'hu') return amount * 1.2
-    return amount * 0.8
+    const portion = this.lookup[category] || 1
+    return amount * portion
   }
 }
+
+// loading csv
+const data: any = {}
+const csvPath = path.join(__dirname, '..', '..', 'assets', 'ppp_indices.csv')
+console.log('Reading ppp csv file from', csvPath)
+
+const file = fs.readFileSync(csvPath, { encoding: 'utf-8' })
+file
+  .toString()
+  .split('\n')
+  .filter((_, i) => i > 0)
+  .forEach((line) => {
+    const sl = line.split(',')
+    const from = sl[1]
+    const to = sl[2]
+    const category = sl[3]
+    const portion = parseFloat(sl[4])
+
+    data[from] = data[from] || {}
+    data[from][to] = data[from][to] || {}
+    data[from][to][category] = portion
+  })
