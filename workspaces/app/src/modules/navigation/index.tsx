00cdf8c6eb21aa6f5c52c70fbcec858306ff1953
makaias <sandorattilamakai@gmail.com>
2022-10-22 20:03:35 +0100

    reworked navigation bar

--- workspaces/app/src/modules/navigation/index.tsx
+++ workspaces/app/src/modules/navigation/index.tsx
@@ -1,7 +1,8 @@
-import { FaUserCircle } from 'react-icons/fa'
+import { FaAngleLeft, FaArrowLeft, FaUserCircle } from 'react-icons/fa'
 import { H1 } from '../h1'
 import Icon from '../icon'
 import { useNavigate } from 'react-router-dom'
+import Logo from './Logo.svg'
 
 export interface NavigationProps {}
 
@@ -9,15 +10,17 @@ export const Navigation = ({}: NavigationProps) => {
   const navigate = useNavigate()
   return (
     <div className="flex w-full justify-between bg-ui-grey">
+      <Icon onClick={() => navigate(-1)}>
+        <FaArrowLeft />
+      </Icon>
+      <H1 variant="medium" className="h-full text-center">
+        <div className="h-full p-2">
+          <img className="h-full" src={Logo} />
+        </div>
+      </H1>
       <Icon onClick={() => navigate('/home')}>
         <FaUserCircle />
       </Icon>
-      <H1 variant="medium" className="h-full text-center">
-        Username
-      </H1>
-      <H1 variant="medium" className="h-full text-center">
-        WISE
-      </H1>
     </div>
   )
 }
