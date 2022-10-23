d381558f52dbe0c3812cf3805855bb2d5bb1d93e
Bendeguz Cseresnyes <cser.bende@gmail.com>
2022-10-23 04:12:13 +0000

    fix comparision spacing

--- workspaces/app/src/pages/comparisonDashboard/PricePairItem.tsx
+++ workspaces/app/src/pages/comparisonDashboard/PricePairItem.tsx
@@ -11,10 +11,12 @@ export const PricePairItem = ({ homePrice, icon, destinationPrice, title }: Pric
   return (
     <div className="flex flex-col w-full">
       <div className="flex w-full justify-center">{title}</div>
-      <div className="flex w-full justify-evenly items-center">
+      <div className="grid grid-cols-3 w-full px-2 justify-between items-center">
         <div>{homePrice}</div>
-        <div>{icon}</div>
-        <div>{destinationPrice}</div>
+        <div className="flex">
+          <div className="mx-auto">{icon}</div>
+        </div>
+        <div className=" text-right">{destinationPrice}</div>
       </div>
     </div>
   )
