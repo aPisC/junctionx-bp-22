51785a4fe07ae1c65e8064b8a199b99d22aae12c
makaias <sandorattilamakai@gmail.com>
2022-10-23 02:59:35 +0100

    basic shopping option

--- workspaces/app/src/pages/abroadHome/index.tsx
+++ workspaces/app/src/pages/abroadHome/index.tsx
@@ -1,12 +1,17 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 import Scrollbars from 'react-custom-scrollbars-2'
 import { useNavigate } from 'react-router-dom'
+import { FlagsRequest } from '../../cache/flagRequest'
 import { BACKEND_URL } from '../../config/backendUrl'
+import Button from '../../modules/button'
 import { H1 } from '../../modules/h1'
+import Modal, { ModalBody, ModalHandler } from '../../modules/modal'
 import Navigation from '../../modules/navigation'
 import Tabs, { TabsHeader, Tab, TabsBody, TabsPanel } from '../../modules/tabs'
+import { useRequest } from '../../utils/useRequest'
 import { useSpinneredRequest } from '../../utils/useSpinneredRequest'
+import { useTriggeredRequest } from '../../utils/useTriggeredRequest'
 import BasePage from '../base'
 import BalanceView from '../home/BalanceView'
 import TransactionItem from '../home/TransactionItem'
@@ -37,12 +42,15 @@ export default function AbroadHomePage({}: AbroadHomePageProps) {
         }),
     []
   )
-
+  const flagsRequest = useRequest(() => FlagsRequest, [])
   if (userRequest.isRunning || transactionsRequest.isRunning) return null
 
+  const [trigger, setTrigger] = useState(false)
   const user: any = userRequest.data
   const mainAccount = user.accounts.find((a: any) => a.type == 'main')
   const saveAccount = user.accounts.find((a: any) => a.type == 'save')
+  const transactions = transactionsRequest.data
+  console.log(flagsRequest)
   return (
     <BasePage>
       <div className="h-full flex flex-col">
@@ -78,8 +86,8 @@ export default function AbroadHomePage({}: AbroadHomePageProps) {
             </div>
             <div className="p-2 flex flex-col gap-2 h-full max-h-[10em] overflow-hidden">
               <Scrollbars>
-                {transactionsRequest.data
-                  ?.filter((tr: any) => tr.amount < 0)
+                {transactions
+                  ?.filter((tr: any) => tr.amount > 0 && tr.accout === saveAccount.id)
                   .map((tr: any) => (
                     <TransactionItem currency={mainAccount.currency} key={tr.id} shop={tr.name} expense={-tr.amount} />
                   ))}
@@ -91,9 +99,78 @@ export default function AbroadHomePage({}: AbroadHomePageProps) {
               </div>
             </div>
             <LineChartView />
+            <div className="w-full flex justify-center p-4">
+              <Modal blur rounded>
+                <ModalHandler>
+                  <Button variant="primary">Imitate shopping</Button>
+                </ModalHandler>
+                <ModalBody title="Imaginary shopping">
+                  {shoppingList.map((item, index) => (
+                    <div className="px-2">
+                      <div
+                        onClick={() =>
+                          axios
+                            .post(`${BACKEND_URL}/api/transaction`, {
+                              amount: item.price,
+                              userId: user.id,
+                              category: item.category,
+                            })
+                            .then(() => {
+                              setTrigger(!trigger)
+                            })
+                        }
+                        key={index}
+                        className="flex w-full justify-between px-4 py-2 border-b border-dark-grey"
+                      >
+                        <div>{item.title}</div>
+                        <div>{item.price}</div>
+                      </div>
+                    </div>
+                  ))}
+                </ModalBody>
+              </Modal>
+            </div>
           </Scrollbars>
         </div>
       </div>
     </BasePage>
   )
 }
+
+const shoppingList = [
+  {
+    title: 'Board games',
+    price: 30,
+    category: 'leisure',
+  },
+  {
+    title: 'Laptop',
+    price: 250,
+    category: 'it-tech',
+  },
+  {
+    title: 'Catfood',
+    price: 10,
+    category: 'food-non-alc',
+  },
+  {
+    title: 'Groceries',
+    price: 43,
+    category: 'food-non-alc',
+  },
+  {
+    title: 'Travel charge',
+    price: 20,
+    category: 'transport',
+  },
+  {
+    title: 'Restaurant + tip',
+    price: 30,
+    category: 'food-non-alc',
+  },
+  {
+    title: 'Nurse outfit',
+    price: 50,
+    category: 'clothing',
+  },
+]
